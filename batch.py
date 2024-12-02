5b4d08f2413fbade6cffbf7da468123ec967c52a
Scott McElroy <scottmcelroy54@gmail.com>
2024-12-02 15:01:42 -0500

    Updating cores per node

--- batch.py
+++ batch.py
@@ -13,7 +13,7 @@ shell_config = {'command': 'mpiexec -np 4 nrniv -python -mpi init.py'}
 # use batch_sge_config if running on Downstate HPC or other SGE systems
 sge_config = {
     'queue': 'cpu.q',
-    'cores': 64,
+    'cores': 32,
     'vmem': '18G',
     'realtime': '01:40:00',
     'command': 'mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py'
