92198f22dc6a412cf04e45a8fd500f6d333d2c43
Scott McElroy <scottmcelroy54@gmail.com>
2025-02-13 13:00:33 -0500

    Tests worked, now time to run full batch

--- cfg.py
+++ cfg.py
@@ -21,7 +21,7 @@ cfg = specs.SimConfig()
 # ------------------------------------------------------------------------------
 # Run parameters
 # ------------------------------------------------------------------------------
-cfg.duration = 500  # Duration of the sim, in ms
+cfg.duration = 3000  # Duration of the sim, in ms
 cfg.dt = 0.05  # Internal Integration Time Step
 cfg.verbose = 0  # Show detailed messages
 cfg.progressBar = 0  # even more detailed message
