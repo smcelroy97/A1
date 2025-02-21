5e6f993f5b6e275c8b57f29c4fe65b65b5c0c660
Scott McElroy <scottmcelroy54@gmail.com>
2025-02-21 10:48:07 -0500

    making this qsub

---
+++ submitMap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -cwd
+#$ -N genMap
+#$ -q cpu.q
+#$ -pe smp 60
+#$ -l h_vmem=494G
+#$ -l h_rt=1:40:00
+#$ -o /ddn/smcelroy97/A1/OUmap.out
+#$ -e /ddn/smcelroy97/A1/OUmap.err
+
+source ~/.bashrc
+mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py
