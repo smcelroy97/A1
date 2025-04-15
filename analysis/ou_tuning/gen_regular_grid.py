42e7c175df7749ed8079dedb39439ebd198458df
Scott McElroy <scottmcelroy54@gmail.com>
2025-04-15 11:59:26 -0400

    Moving the initial mean value lower, first batch had too much firing

--- analysis/ou_tuning/gen_regular_grid.py
+++ analysis/ou_tuning/gen_regular_grid.py
@@ -30,10 +30,10 @@ def gen_regular_grid(
 if __name__ == '__main__':
     
     # Define the ranges and number of points for the grid
-    ou_mean_range = (-0.001, 0.015)
-    ou_mean_npoints = 20
+    ou_mean_range = (-0.01, 0.015)
+    ou_mean_npoints = 30
     ou_std_range = (0, 0.03)
-    ou_std_npoints = 20
+    ou_std_npoints = 30
 
     # Experiment name
     exp_name = 'batch_ougrid_ngf_0'
