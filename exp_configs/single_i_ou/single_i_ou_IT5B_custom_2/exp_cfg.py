38f9a8ef61a84d5c1ac12b49c3dbb95ff1b0346c
Nikita Novikov <nikknovikov@gmail.com>
2025-06-24 15:43:12 -0400

    Add exp's for pre-/post-stim comparison in unconn pop with OU increase over cells

--- exp_configs/single_i_ou/single_i_ou_IT5B_custom_2/exp_cfg.py
+++ exp_configs/single_i_ou/single_i_ou_IT5B_custom_2/exp_cfg.py
@@ -8,10 +8,11 @@ dirpath_self = Path(__file__).resolve().parent
 def apply_exp_cfg(cfg):
 
     # Duration
-    cfg.duration = 10 * 1e3
+    cfg.duration = 5 * 1e3
     
-    # Turn off the connections
+    # Turn off the connections and bkg inputs
     cfg.addConn = 0
+    cfg.addBkgConn = 0
 
     # Populations to use
     cfg.pops_active = [
@@ -30,15 +31,20 @@ def apply_exp_cfg(cfg):
     cfg.ou_common = 1    # all pops receive the same OU input
     cfg.ou_noise_duration = cfg.duration
     cfg.ou_tau = 2
-    #cfg.OUamp = [0, 0.025]
+    cfg.OUamp = [0, 0.03]
+    cfg.OUstd = 0
     #cfg.OUstd = [(x + 0.005) * 0.5 for x in cfg.OUamp]
-    cfg.OUamp = 0.007
-    cfg.OUstd = [0, 0.015]
+    #cfg.OUamp = 0.005
+    #cfg.OUstd = [0, 0.03]
 
     # Record voltage traces
     cells_rec = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450]
     cfg.recordCells = [(pop, cells_rec) for pop in cfg.allpops]
-    cfg.recordTraces = {'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'}}
+    cfg.recordTraces = {
+        'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'},
+        #'p_kBK': {'sec': 'soma', 'loc': 0.5, 'var': 'p', 'mech': 'kBK'}
+        'n_kdr': {'sec': 'soma', 'loc': 0.5, 'var': 'n', 'mech': 'kdr'}
+    }
     cfg.recordStep =  0.1
     cfg.analysis['plotTraces'] = {
         'include': [(pop, cells_rec) for pop in cfg.allpops],
@@ -48,10 +54,21 @@ def apply_exp_cfg(cfg):
     }
 
     # OU ramp
-    cfg.ou_ramp_t0 = 2000
     #cfg.ou_ramp_dur = None
-    cfg.ou_ramp_dur = 2000
-    cfg.ou_ramp_offset = 0.2
+    cfg.ou_ramp_dur = 1000
+    cfg.ou_ramp_t0 = 1500
+    cfg.ou_ramp_offset = 3
     cfg.ou_ramp_mult = 1
     #cfg.ou_ramp_type = 'up_down'
     cfg.ou_ramp_type = 'up'
+
+
+def modify_net_params(cfg, params):
+    
+    kbk = params.cellParams['IT5B_reduced']['secs']['soma'] ['mechs']['kBK']
+    kdr = params.cellParams['IT5B_reduced']['secs']['soma'] ['mechs']['kdr']
+    #kbk['gpeak'] = 5e-03
+    #kbk['tau'] = 20
+    kdr['gbar'] = 0.04
+
+    #pass
