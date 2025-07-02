39834ab168ab6bdac4051e4aa5bb2be2a48f0f94
Scott McElroy <scottmcelroy54@gmail.com>
2025-07-02 12:06:53 -0400

    final touches for the csv prep

--- analysis/psp_tools.py
+++ analysis/psp_tools.py
@@ -6,8 +6,16 @@ import numpy as np
 import csv
 from mpi4py import MPI
 
+batch_dir = '../simOutput/PSPTest/'
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+
+files = [f for f in os.listdir(batch_dir) if f.endswith('.pkl')]
+files.sort()  # Ensure consistent order
+
+# Distribute files among ranks
+my_files = files[rank::comm.Get_size()]
 
 save_csv = 1
 plot_psp_secs = 0
@@ -15,90 +23,65 @@ plot_avg_vs_secs = 0
 plot_psp_matrix = 0
 plot_psp_box = 0
 
-batch_dir = '../simOutput/PSPTest/'
 
 pop_psps = {}
 psp_amps = {}
 
-for file in os.listdir(batch_dir):
-    if file.endswith('.pkl'):
-        with open(batch_dir + file, 'rb') as f:
-            sim_results = pickle.load(f)
 
-        prePop = sim_results['simConfig']['prePop']
-        # Extract pop, cell type, and sections
-        for pop in sim_results['net']['params']['popParams']:
-            if '_stim' in pop:
-                continue
-            pop_psps[pop] = {}
-            pop_psps[pop][prePop + '_' + pop] = {}
-            pop_psps[pop][prePop + '_' + pop]['secs'] = {}
+for file in my_files:
+    with open(batch_dir + file, 'rb') as f:
+        sim_results = pickle.load(f)
 
-            # Get the section specific delays
-            for conn in sim_results['net']['params']['connParams'].values():
-                if pop == conn['postConds']['pop']:
-                    pop_psps[pop][prePop + '_' + pop]['secs'][conn['sec']] = {}
-                    pop_psps[pop][prePop + '_' + pop]['secs'][conn['sec']]['delay'] = conn['delay']
+    prePop = sim_results['simConfig']['prePop']
+    print(prePop)
+    # Extract pop, cell type, and sections
+    for pop in sim_results['net']['params']['popParams']:
+        if '_stim' in pop:
+            continue
+        pop_psps[pop] = {}
+        pop_psps[pop][prePop] = {}
+        pop_psps[pop][prePop]['secs'] = {}
 
-            # Extract gid for pop (for single cell batch)
-            for cell in sim_results['net']['cells']:
-                if cell['tags']['pop'] == pop:
-                    pop_psps[pop][prePop + '_' + pop]['gid'] = cell['gid']
-                    break
+        # Get the section specific delays
+        for conn in sim_results['net']['params']['connParams'].values():
+            if pop == conn['postConds']['pop']:
+                pop_psps[pop][prePop]['secs'][conn['sec']] = {}
+                pop_psps[pop][prePop]['secs'][conn['sec']]['delay'] = conn['delay']
 
-            # Based on the section delay, extract the slice of the somatic trace
-            baseline = 50
-            post_stim = 1500  # ms after stimulus
-            for sec in pop_psps[pop][prePop + '_' + pop]['secs'].values():
-                post_window = int(post_stim / sim_results['simConfig']['dt'])
-                baseline_window = int(post_stim / sim_results['simConfig']['dt'])
-                stim_idx = int(sec['delay'] / sim_results['simConfig']['dt'])
-                start = stim_idx - baseline_window
-                end = stim_idx + post_window
-                trace = sim_results['simData']['V_soma']['cell_' + str(pop_psps[pop][prePop + '_' + pop]['gid'])]
-                sec['trace'] = trace[start:end]
+        # Extract gid for pop (for single cell batch)
+        for cell in sim_results['net']['cells']:
+            if cell['tags']['pop'] == pop:
+                pop_psps[pop][prePop]['gid'] = cell['gid']
+                break
 
-                baseline_mv = np.mean(trace[:baseline_window])
-                if prePop in sim_results['simConfig']['Epops'] + sim_results['simConfig']['TEpops']:
-                    peak = np.max(trace[baseline_window:])
-                else:
-                    peak = np.min(trace[baseline_window:])
-                amplitude = peak - baseline
-                sec['psp'] = amplitude
+        # Based on the section delay, extract the slice of the somatic trace
+        baseline = 50
+        post_stim = 1500  # ms after stimulus
+        sec_amps = []
+        for sec in pop_psps[pop][prePop]['secs'].values():
+            post_window = int(post_stim / sim_results['simConfig']['dt'])
+            baseline_window = int(post_stim / sim_results['simConfig']['dt'])
+            stim_idx = int(sec['delay'] / sim_results['simConfig']['dt'])
+            start = stim_idx - baseline_window
+            end = stim_idx + post_window
+            trace = sim_results['simData']['V_soma']['cell_' + str(pop_psps[pop][prePop]['gid'])]
+            sec['trace'] = trace[start:end]
 
-# Gather all pop_psps dicts to rank 0
-all_pop_psps = comm.gather(pop_psps, root=0)
+            baseline_mv = np.mean(trace[:baseline_window])
+            if prePop in sim_results['simConfig']['Epops'] + sim_results['simConfig']['TEpops']:
+                peak = np.max(trace[baseline_window:])
+            else:
+                peak = np.min(trace[baseline_window:])
+            amplitude = peak - baseline
+            sec['psp'] = amplitude
+            sec_amps.append(amplitude)
+        pop_psps[pop][prePop]['secs']['mean'] = np.mean(sec_amps)
 
-if save_csv and rank == 0:
-    # Merge all dictionaries
-    merged_pop_psps = {}
-    for d in all_pop_psps:
-        for pop, prePop_dict in d.items():
-            if pop not in merged_pop_psps:
-                merged_pop_psps[pop] = {}
-            merged_pop_psps[pop].update(prePop_dict)
-
-    with open(f'{batch_dir}pop_psps.csv', 'w', newline='') as csvfile:
-        fieldnames = ['pop', 'prePop', 'sec', 'delay', 'gid', 'psp']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for pop, prePop_dict in pop_psps.items():
-            for prePop_pop, data in prePop_dict.items():
-                gid = data.get('gid', None)
-                for sec, sec_data in data['secs'].items():
-                    writer.writerow({
-                        'pop': pop,
-                        'prePop': prePop_pop,
-                        'sec': sec,
-                        'delay': sec_data.get('delay', None),
-                        'gid': gid,
-                        'psp': sec_data.get('psp', None)
-                    })
 
 if plot_psp_secs:
     # Set up plotting for all sec traces and psps
     for pop, prePop_dict in pop_psps.items():
-        for prePop_pop, data in prePop_dict.items():
+        for prePop, data in prePop_dict.items():
             pop_dir = os.path.join(batch_dir, f'plots/{pop}_plots/{prePop}->{pop}')
             os.makedirs(pop_dir, exist_ok=True)
             traces = []
@@ -120,7 +103,7 @@ if plot_psp_secs:
 
 if plot_avg_vs_secs:
     for pop, prePop_dict in pop_psps.items():
-        for prePop_pop, data in prePop_dict.items():
+        for prePop, data in prePop_dict.items():
             pop_dir = os.path.join(batch_dir, f'plots/{pop}_plots/{prePop}->{pop}')
             os.makedirs(pop_dir, exist_ok=True)
             traces = []
@@ -148,7 +131,7 @@ if plot_avg_vs_secs:
 
 if plot_psp_box:
     for pop, prePop_dict in pop_psps.items():
-        for prePop_pop, data in prePop_dict.items():
+        for prePop, data in prePop_dict.items():
             pop_dir = os.path.join(batch_dir, f'plots/{pop}_plots/{prePop}->{pop}')
             os.makedirs(pop_dir, exist_ok=True)
 
@@ -159,3 +142,43 @@ if plot_psp_box:
                 plt.ylabel('Amplitude (mV)')
                 # plt.savefig(os.path.join(pop_dir, f'psp_amplitudes_summary.png'))
                 plt.close()
+
+
+def strip_traces(d):
+    for pop in d:
+        for prePop in d[pop]:
+            for sec in d[pop][prePop]['secs']:
+                if 'trace' in d[pop][prePop]['secs'][sec]:
+                    del d[pop][prePop]['secs'][sec]['trace']
+
+
+strip_traces(pop_psps)
+
+# Gather all pop_psps dicts to rank 0
+all_pop_psps = comm.gather(pop_psps, root=0)
+
+if save_csv and rank == 0:
+    # Merge all dictionaries
+    merged_pop_psps = {}
+    for d in all_pop_psps:
+        for pop, prePop_dict in d.items():
+            if pop not in merged_pop_psps:
+                merged_pop_psps[pop] = {}
+            merged_pop_psps[pop].update(prePop_dict)
+
+    with open(f'{batch_dir}pop_psps.csv', 'w', newline='') as csvfile:
+        fieldnames = ['pop', 'prePop', 'sec', 'delay', 'gid', 'psp']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for pop, prePop_dict in merged_pop_psps.items():  # <-- use merged_pop_psps here
+            for prePop, data in prePop_dict.items():
+                gid = data.get('gid', None)
+                for sec, sec_data in data['secs'].items():
+                    writer.writerow({
+                        'pop': pop,
+                        'prePop': prePop,
+                        'sec': sec,
+                        'delay': sec_data.get('delay', None),
+                        'gid': gid,
+                        'psp': sec_data.get('psp', None)
+                    })
