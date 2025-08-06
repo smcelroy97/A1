7785ba69c3f1c1dfdd797c889bffdb049c7406e3
niknovikov19 <nikknovikov@gmail.com>
2025-08-06 19:36:13 -0400

    First test - single_i_ou_unconn_state_1_nosub

---
+++ exp_configs/single_i_ou_unconn_state_1_nosub/INFO.md
@@ -0,0 +1,6 @@
+- Constant OU current input (mean, std), see `ou_inputs.json`, tau=10.
+- Isolated populations in a surrogate surrounding (no recurrent connections).
+- Surrogate state: `state_1` (see `target_state_1.csv`).
+- Subconn is turned off.
+- For exc. cells, gKDR is increased to avoid cell-level multistability (see `mech_changes_1.json`)
+    It distorts f-I curves compared to experimental data, re-tuning is needed in future.
---
+++ exp_configs/single_i_ou_unconn_state_1_nosub/it2_pv2_wmult_0.1_test/exp_cfg.py
@@ -0,0 +1,69 @@
+import json
+import os
+from pathlib import Path
+import shutil
+
+
+dirpath_self = Path(__file__).resolve().parent
+
+
+def apply_exp_cfg(cfg):
+
+    # Duration
+    cfg.duration = 3 * 1e3
+
+    # Populations to use
+    cfg.pops_active = ['IT2', 'PV2']
+
+    # Subnet parameters
+    cfg.subnet_build_flag = 1
+    cfg.subnet_params = {
+        'pops_active': cfg.pops_active,   
+        'conns_frozen': 'all',   # all inputs are surrogate, no recurrent connections
+        'fpath_frozen_rates': str(dirpath_self / 'target_state_1.csv'),   # surrogate input
+    }
+
+    # Global wight multiplier
+    cfg.wmult = 0.1
+
+    # Turn off subConn
+    cfg.addSubConn = 0
+
+    # OU current input
+    cfg.add_ou_current = 1
+    cfg.ou_common = 0
+    cfg.ou_noise_duration = cfg.duration
+    cfg.ou_tau = 10
+    with open(dirpath_self / 'ou_inputs.json', 'r') as fid:
+        cfg.ou_pop_inputs = json.load(fid)
+
+    # Cell mechanisms to modify
+    with open(dirpath_self / 'mech_changes_1.json', 'r') as fid:
+        cfg.mech_changes = json.load(fid)
+
+    cfg.allpops = cfg.pops_active
+    if 'plotRaster' in cfg.analysis:
+        cfg.analysis['plotRaster']['include'] = cfg.pops_active
+    if 'plotSpikeStats' in cfg.analysis:
+        cfg.analysis['plotSpikeStats']['include'] = cfg.pops_active
+    if 'plotTraces' in cfg.analysis:
+        cfg.analysis['plotTraces']['include'] = cfg.pops_active
+    
+    # Time range for rate and CV calculation
+    cfg.analysis['plotSpikeStats']['timeRange'] = [cfg.duration - 1000, cfg.duration]
+    #cfg.analysis['plotSpikeStats'] = False
+
+
+def modify_net_params(cfg, params):
+    """Applied after netParams creation. """
+
+    # Modify membrane mechanisms
+    for v in cfg.mech_changes.values():
+        secs_all = params.cellParams[v['pop']]['secs']
+        if v['sec'] == 'all':
+            secs = list(secs_all.values())
+        else:
+            secs = [secs_all[v['sec']]]
+        for sec in secs:
+            sec['mechs'][v['mech']][v['par']] *= v['mult']
+            sec['mechs'][v['mech']][v['par']] += v['add']
