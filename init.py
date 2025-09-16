fixing init loss calculation

init.py
@@ -139,13 +139,13 @@ if sim.rank == 0:
     results[pop] = rate
     target = 5.0 if pop in Ipops else 1.0
 
-    # Calculate MSE for E and I populations
-    e_losses = [(results[pop] - 1.0) ** 2 for pop in Epops]
-    i_losses = [(results[pop] - 5.0) ** 2 for pop in Ipops]
+  # Calculate MSE for E and I populations
+  e_losses = [(results[pop] - 1.0) ** 2 for pop in Epops]
+  i_losses = [(results[pop] - 5.0) ** 2 for pop in Ipops]
 
-    results['e_loss_avg'] = np.mean(e_losses) if e_losses else 0
-    results['i_loss_avg'] = np.mean(i_losses) if i_losses else 0
-    results['loss'] = (results['e_loss_avg'] + results['i_loss_avg']) / 2
+  results['e_loss_avg'] = np.mean(e_losses) if e_losses else 0
+  results['i_loss_avg'] = np.mean(i_losses) if i_losses else 0
+  results['loss'] = (results['e_loss_avg'] + results['i_loss_avg']) / 2
 
   out_json = json.dumps({**inputs, **results})
   sim.send(out_json)