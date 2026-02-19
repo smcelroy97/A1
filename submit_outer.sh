60fdb4e764ea8b617eb0c582571571464dbb773f
Scott McElroy <scottmcelroy54@gmail.com>
2026-02-19 16:53:23 -0500

    inner not upping init usage, switching to outer for core request

--- submit_outer.sh
+++ submit_outer.sh
@@ -3,8 +3,8 @@
 #SBATCH -A TG-MED240050
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=4G
+#SBATCH --ntasks=64
+#SBATCH --mem=200G
 #SBATCH --time=48:00:00
 #SBATCH --output=outer_%j.out
 #SBATCH --error=outer_%j.err
