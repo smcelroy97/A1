6e5dd7c780e35282cf66aea72990e56ca4916666
Scott McElroy <scottmcelroy54@gmail.com>
2026-02-23 15:07:34 -0500

    Getting rid of cfg_base and related imports

--- cfg.py
+++ cfg.py
@@ -1,39 +1,163 @@
-from pathlib import Path
-
+from netpyne.batchtools import specs
 import numpy as np
-import pandas as pd
 
-from cfg_base import cfg
+cfg = specs.SimConfig()
+
+# ------------------------------------------------------------------------------
+# Run parameters
+# ------------------------------------------------------------------------------
+cfg.duration = 7 * 1e2  # Duration of the sim, in ms
+cfg.dt = 0.05                  ## Internal Integration Time Step
+cfg.verbose = False         	## Show detailed messages
+cfg.hParams['celsius'] = 37
+cfg.createNEURONObj = 1
+cfg.createPyStruct = 1
+cfg.printRunTime = 0.1
+
+cfg.connRandomSecFromList = False  # set to false for reproducibility
+cfg.cvode_active = False
+cfg.cvode_atol = 1e-6
+cfg.cache_efficient = True
+cfg.oneSynPerNetcon = False
+cfg.includeParamsLabel = False
+#cfg.printPopAvgRates = [0, cfg.duration] # don't use cfg.duration as a reference structure...
+cfg.validateNetParams = False
+
+cfg.seeds['stim'] = 1
+cfg.seeds['conn'] = 1
+
+# ------------------------------------------------------------------------------
+# Recording
+# ------------------------------------------------------------------------------
 
 
-# Duration
-cfg.duration = 7 * 1e2
+# original code ---
+# 359 'IT5A' cells ---
 
-# Turn off the connections
-cfg.addConn = 0
+# Record voltage traces
+cfg.recordCells = [['IT5A', [*range(359)]]]
+cfg.recordTraces = {
+    'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'}
+}
+cfg.recordStep =  0.1
+cfg.recordStim = False
+cfg.recordTime = True
+cfg.recordLFP = False
+cfg.recordDipole = False
 
-# Populations to use
-POP_ACTIVE = 'IT5A'
-cfg.pops_active = [POP_ACTIVE]
-cfg.allpops = cfg.pops_active
-if 'plotRaster' in cfg.analysis:
-    cfg.analysis['plotRaster']['include'] = cfg.pops_active
-if 'plotSpikeStats' in cfg.analysis:
-    cfg.analysis['plotSpikeStats']['include'] = cfg.pops_active
-if 'plotTraces' in cfg.analysis:
-    cfg.analysis['plotTraces']['include'] = cfg.pops_active
+
+# ------------------------------------------------------------------------------
+# Saving
+# ------------------------------------------------------------------------------
+cfg.simLabel = 'basic_sim'
+cfg.saveFolder = 'batch'
+cfg.savePickle = True
+cfg.saveJson = True
+cfg.saveDataInclude = ['simData', 'simConfig', 'net', 'netParams',  'netCells', 'netPops']
+cfg.backupCfgFile = None
+cfg.gatherOnlySimData = False
+cfg.saveCellSecs = False
+cfg.saveCellConns = False
+
+# ------------------------------------------------------------------------------
+# Analysis and plotting
+# ------------------------------------------------------------------------------
+cfg.analysis['plotRaster'] = {
+    'include': ['IT5A'],
+    'saveFig': True,
+    'showFig': False,
+    'orderInverse': True,
+    'figSize': (25, 25),
+    'markerSize': 1
+}
 
 cfg.analysis['plotSpikeStats'] = False
 
-# Constant input increasing across the cells
-# ("I" of the f-I curve)
-cfg.add_ou_current = 1
-cfg.ou_common = 1    # all pops receive the same OU input
-cfg.ou_noise_duration = cfg.duration
-cfg.ou_tau = 10
-cfg.OUamp = [-0.02, 0.1]   # interpolate between 2 values from 1st to last cell
-cfg.OUstd = 0   # zero std. -> constant input
+# ------------------------------------------------------------------------------
+# Cells
+# ------------------------------------------------------------------------------
+cfg.weightNormThreshold = 5.0
+cfg.weightNormScaling = {'NGF_reduced': 1.0, 'ITS4_reduced': 1.0}
+cfg.ihGbar = 1.0
+cfg.KgbarFactor = 1.0
 
+# ------------------------------------------------------------------------------
+# Synapses
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Network
+# ------------------------------------------------------------------------------
+cfg.singleCellPops = False
+cfg.reducedPop = False
+cfg.singlePop = ''
+cfg.removeWeightNorm = False
+
+# ------------------------------------------------------------------------------
+# Connectivity
+# ------------------------------------------------------------------------------
+cfg.addConn = 0
+cfg.addSubConn = 1
+cfg.wireCortex = 1
+cfg.wmult = 1.0
+cfg.addIntraThalamicConn = 1
+cfg.addCorticoThalamicConn = 1
+cfg.addThalamoCorticalConn = 1
+
+# ------------------------------------------------------------------------------
+# Background inputs
+# ------------------------------------------------------------------------------
+cfg.addBkgConn = 0
+cfg.noiseBkg = 1.0
+cfg.delayBkg = 5.0
+cfg.startBkg = 0
+cfg.rateBkg = {'exc': 40, 'inh': 40}
+cfg.EbkgThalamicGain = 1.0
+cfg.IbkgThalamicGain = 1.0
+
+# ------------------------------------------------------------------------------
+# Current inputs
+# ------------------------------------------------------------------------------
+cfg.addIClamp = False
+cfg.addRIClamp = False
+
+# ------------------------------------------------------------------------------
+# OU current or conductance inputs
+# -----------------------------------------------------------------------------
+cfg.add_ou_current = 1
+cfg.add_ou_conductance = 0
+cfg.ou_common = 1
+cfg.ou_tau = 10
+cfg.ou_noise_duration = cfg.duration
+cfg.OUamp = [-0.02, 0.18]
+cfg.OUstd = 0
+
+# ------------------------------------------------------------------------------
+# NetStim inputs
+# ------------------------------------------------------------------------------
+cfg.addNetStim = False
+
+# NetStim inputs (weak, just to randomly jitter the cells between steady-states)
+cfg.bkg_r = 75    # firing rate
+cfg.bkg_w = 0.5   # weight
+
+# Strong ramp-up pulse for switching between the steady-states
+cfg.ou_ramp_dur = 1000   # duration
+cfg.ou_ramp_t0 = 3500    # start time
+cfg.ou_ramp_offset = 1.5   # amplitude (current to soma)
+cfg.ou_ramp_mult = 0
+cfg.ou_ramp_type = 'up'
+
+cfg.batch_id = 0 # assist labelling
+
+# Mechanisms to modify...
+# see in netParams.py multiply_parameters_func ...
+# def multiply_parameters_func(pop: str,
+#                              secs: list,
+#                              mech: str,
+#                              parameter: str,
+#                              factor: int|float):
+# 'kdr0' and 'cal0', labels, dictionary of 'secs', 'mech', 'parameter', 'factor' form the kwargs...
 
 cfg.multiply_parameters = {
     'kdr0': {
@@ -44,54 +168,4 @@ cfg.multiply_parameters = {
     }
 }
 
-# Strong ramp-up pulse for switching between the steady-states
-cfg.ou_ramp_dur = 1000   # duration
-cfg.ou_ramp_t0 = 3500    # start time
-cfg.ou_ramp_offset = 1.75   # amplitude (current to soma)
-cfg.ou_ramp_mult = 0
-cfg.ou_ramp_type = 'up'
-
-cfg.batch_id = 0 
-
-# NetStim inputs (weak, just to randomly jitter the cells between steady-states)
-cfg.bkg_r = 150    # firing rate
-cfg.bkg_w = 0.5   # weight
-cfg.bkg_spike_inputs = {
-    pop: {'r': cfg.bkg_r, 'w': cfg.bkg_w}
-    for pop in cfg.pops_active
-}
-
-# Experiment folder and name
-# cfg.saveFolder = 'sim_output/' + (
-#     f'sim_{POP_ACTIVE}_kdr0'
-#     f'_mult_{cfg.multiply_parameters["kdr0"]["factor"]}'
-#     f'_ouamp_{cfg.OUamp[0]}_{cfg.OUamp[1]}'
-# )
-# cfg.saveFolder = 'batch'
-cfg.simLabel = '00005'
-
-# Load a table of pop sizes
-dirpath_self = Path(__file__).resolve().parent
-fpath_csv = dirpath_self / 'pops_sz.csv'
-pops_sz_df = pd.read_csv(fpath_csv)
-pops_sz = pops_sz_df.set_index('pop')['ncells'].to_dict()
-
-# Choose the cells to record voltages for each active pop.
-ncells_rec = 500
-cfg.pop_cells_rec = {}
-for pop in cfg.allpops:
-    N = np.minimum(pops_sz[pop], ncells_rec)
-    cfg.pop_cells_rec[pop] = np.linspace(0, pops_sz[pop] - 1, N, dtype=int)
-
-# Record voltage traces
-cfg.recordCells = [(pop, list(cfg.pop_cells_rec[pop]))
-                    for pop in cfg.allpops]
-cfg.recordTraces = {
-    'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'}
-}
-cfg.recordStep = 0.1
-
-cfg.backupCfgFile = [f"{cfg.saveFolder}/{cfg.simLabel}_cfg.json"]
-
-# Update via batchtools
 cfg.update()
